2903af71a3890d867f9cb1175bb842b2f778656a
palapajr <qumfaanzirr@gmail.com>
2023-04-26 13:14:25 +0700

    push

--- application/views/view-login.php
+++ application/views/view-login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Login CMS 2023</title>
+    <title>Login SMPC 2023</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/bootstrap/css/bootstrap.min.css">
@@ -41,6 +41,7 @@
                         <div class="login-brand">
                             <img src="<?php echo base_url(); ?>assets/img/logo.png" alt="logo" width="100" class="shadow-light rounded-circle">
                         </div>
+                        <H2 class="text-center">SISTEM MANAJEMEN PELAPORAN COMPLAIN</H2>
 
                         <div class="card card-info">
                             <div class="card-header">
@@ -74,7 +75,7 @@
                         </div>
 
                         <div class="simple-footer">
-                            Copyright &copy; CMS 2023
+                            Copyright &copy; SMPC 2023
                         </div>
                     </div>
                 </div>
